32777ded6096677a0b663465bf01d39e0b2674d5
mafiya-official <emalshadilruvanchamikara.ff@gmail.com>
2025-11-28 09:27:42 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,5 +5,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "LQkV0C5T#SA461DLpOaya-dYmlOJR6G3wH_oo4snCfsc7GuCb9h8",
+SESSION_ID: process.env.SESSION_ID || "",
 };
